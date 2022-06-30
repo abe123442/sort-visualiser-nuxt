89ec16cb40ad11f9486cd5adfff3c9a87af6e7b1
abe123442 <ae248abraham@gmail.com>
2022-06-30 18:49:08 +1000

    Prototype V0.5
    Fixed pause button issue

--- components/SortVisualiser.vue
+++ components/SortVisualiser.vue
@@ -52,8 +52,14 @@
 					<!-- debug button -->
 					<button
 						class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-10 border-b-4 border-blue-700 hover:border-blue-500 rounded mx-10"
-						type="button" id="btnDebug" @click="toggleDebug()" v-if="debugging">
+						type="button" id="btnDebug" @click="toggleDebug()" v-if="!debugging">
 						Pause
+					</button>
+
+					<button
+						class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-10 border-b-4 border-blue-700 hover:border-blue-500 rounded mx-10"
+						type="button" id="btnDebug" @click="toggleDebug()" v-if="debugging">
+						Resume
 					</button>
 					<!-- previous debug button -->
 
